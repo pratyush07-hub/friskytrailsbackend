d42992169b68875a5e81f3a8988b4705b1812c05
Pratyush <choudharypratyush809@gmail.com>
2025-09-07 17:09:21 +0530

    updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -251,7 +251,7 @@ const googleAuth = asyncHandler(async (req, res) => {
       secure: process.env.NODE_ENV === "production",
       sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
-    };
+    }
 
     return res
       .status(200)
