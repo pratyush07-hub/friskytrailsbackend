af43536c82089547de362efebaf2f2b5f409f465
Pratyush <choudharypratyush809@gmail.com>
2025-10-17 23:20:39 +0530

    CORS ORIGIN handeled

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ dotenv.config({
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  process.env.CORS_ORIGIN,
   "https://frisky-trails.vercel.app",
+  // process.env.CORS_ORIGIN,
 ];
 
 app.use((req, res, next) => {
