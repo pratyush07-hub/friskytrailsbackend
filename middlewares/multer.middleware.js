7c944b875af28c80b292a26dcfcc2c9e26cd46b2
Pratyush <choudharypratyush809@gmail.com>
2025-10-24 23:42:11 +0530

    multer changed

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -3,4 +3,11 @@ import multer from "multer";
 // Store file in memory (req.file.buffer)
 const storage = multer.memoryStorage();
 
-export const upload = multer({ storage });
+// Multer config with increased limits
+export const upload = multer({ 
+  storage,
+  limits: {
+    fieldSize: 10 * 1024 * 1024, // 10 MB per text field
+    fileSize: 5 * 1024 * 1024,   // optional: 5 MB per file
+  }
+});
